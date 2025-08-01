d670d4c5c3c1ffb8b9728c86c000011a64129b41
Aun <aunmohammad254@gmail.com>
2025-08-02 02:59:35 +0500

    start

---
+++ add.js
@@ -0,0 +1,8 @@
+function add(c) {
+    a = 50
+    b = 30
+    c = a + b 
+    return c
+}
+add(c)
+module.exports = add
---
+++ divide.js
@@ -0,0 +1,8 @@
+function divide(c) {
+    a = 50
+    b = 30
+    c = a / b 
+    return c
+}
+divide(c)
+module.exports = divide
